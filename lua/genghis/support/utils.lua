a9107372cd706c0aad57a8f3d307a84f667d356f
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-11-17 14:49:44 +0100

    improv(notifications): icon support for `nvim-notify`

--- lua/genghis/support/utils.lua
+++ lua/genghis/support/utils.lua
@@ -14,6 +14,13 @@ function M.notify(msg, level, opts)
 	if not level then level = "info" end
 	if not opts then opts = {} end
 	opts.title = opts.title and "Genghis: " .. opts.title or "Genghis"
+
+	-- since nvim-notify does not support the `icon` field that snacks.nvim
+	if package.loaded["notify"] then
+		opts.title = vim.trim(opts.icon .. opts.title)
+		opts.icon = nil
+	end
+
 	vim.notify(msg, vim.log.levels[level:upper()], opts)
 end
 
