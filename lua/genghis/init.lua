8d7d90c37a00f62c31371e137c75ef7220ff1cb4
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-12-17 13:16:57 +0100

    fix: error on completion of subcommands

--- lua/genghis/init.lua
+++ lua/genghis/init.lua
@@ -16,7 +16,6 @@ vim.api.nvim_create_user_command("Genghis", function(ctx) M[ctx.args]() end, {
 		local allOps = {}
 		vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.file")))
 		vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.copy")))
-		vim.list_extend(allOps, vim.tbl_keys(require("genghis.operations.other")))
 		return vim.tbl_filter(function(op) return op:lower():find(query, nil, true) end, allOps)
 	end,
 })
