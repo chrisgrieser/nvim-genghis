8861405737789e926ae15751c0b6e76490cb63e6
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-11-25 16:56:12 +0100

    fix: error message when misspelling operation

--- lua/genghis/init.lua
+++ lua/genghis/init.lua
@@ -15,11 +15,26 @@ function M.navigateToFileInFolder(direction)
 	require("genghis.operations.navigation").fileInFolder(direction)
 end
 
+-- redirect calls to this module to the respective submodules
 setmetatable(M, {
 	__index = function(_, key)
 		return function(...)
-			local module = vim.startswith(key, "copy") and "copy" or "file"
-			require("genghis.operations." .. module)[key](...)
+			local warn = require("various-textobjs.utils").warn
+
+			local fileOps = vim.tbl_keys(require("genghis.operations.file"))
+			local copyOps = vim.tbl_keys(require("genghis.operations.copy"))
+
+			local module
+			if vim.tbl_contains(fileOps, key) then module = "file" end
+			if vim.tbl_contains(copyOps, key) then module = "copy" end
+
+			if module then
+				require("genghis.operations." .. module)[key](...)
+			else
+				local msg = ("There is no operation called `%s`.\n\n"):format(key)
+					.. "Make sure it exists in the list of operations, and that you haven't misspelled it."
+				warn(msg)
+			end
 		end
 	end,
 })
